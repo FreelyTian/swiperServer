0a9f6985a0487dfb4090ed1e3f1d27dc428da733
freelytian <osk9ehdms01@gmail.com>
2024-04-01 18:04:05 -0300

    Arquivo pra guardar as configurações locais

---
+++ lib/configs/app_settings.dart
@@ -0,0 +1,40 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppSettings {
+  late SharedPreferences _prefs;
+  Map<String, String> localS = {'id': '0'};
+
+  AppSettings() {
+    _startSettings();
+  }
+
+  _startSettings() async {
+    await _startPreferences();
+    await _readSettings();
+  }
+
+  Future<void> _startPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  _readSettings() {
+    final id = _prefs.getString('id') ?? '0';
+    localS = {'id': id};
+  }
+
+  setlocalS(String id) async {
+    await _prefs.setString('id', id);
+  }
+
+  Future<bool> isEmpty() async { 
+    await _startPreferences();
+    String id = _prefs.getString('id') ?? '0';
+    return id == '0';
+  }
+
+  readData() async {
+    await _readSettings();
+    print("id: ${localS['id']}");
+    return localS['id'] ?? 'id não encontrado';
+  }
+}
