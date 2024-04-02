ce2b1405a54938b3a7ade1fff9673421447fdbea
freelytian <osk9ehdms01@gmail.com>
2024-04-02 16:31:41 -0300

    Reestruturação total da tela, ela vai virar só um componente

--- lib/pages/qr_page.dart
+++ lib/pages/qr_page.dart
@@ -1,8 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:swiper_server/helpers/sup.dart';
 
 class QrPage extends StatelessWidget {
   final String id;
@@ -10,40 +8,32 @@ class QrPage extends StatelessWidget {
   // ignore: prefer_const_constructors_in_immutables
   QrPage({super.key, required this.id});
 
- void initialize() async {
-   WidgetsFlutterBinding.ensureInitialized();
-    
- }
-
- void insert() async {
-  final supabase = Supabase.instance.client;
-  await supabase.from('pc_info').insert({'id': id});
-}
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Leia código QR abaixo para se conectar:', style: TextStyle(fontSize: 24)),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical:8.0),
-              child: QrImageView(
-                data: id,
-                version: QrVersions.auto,
-                size: 200,
-                backgroundColor: Colors.white,
-              ),
-            ),
-            ElevatedButton(onPressed: () => {
-              initialize(),
-              insert()
-            }, child: const Text('Registrar'))
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: QrImageView(
+            data: id,
+            version: QrVersions.auto,
+            size: 200,
+            backgroundColor: Colors.white,
+          ),
         ),
-      ),
+        ElevatedButton(
+            onPressed: () => {
+                  if (SupabaseManager().initialized)
+                    {SupabaseManager().insert('pc_info', 'id', id)}
+                  else
+                    {
+                      SupabaseManager().initialize(),
+                      SupabaseManager().insert('pc_info', 'id', id)
+                    }
+                },
+            child: const Text('Registrar', style: TextStyle(fontSize: 24)))
+      ],
     );
   }
 }
